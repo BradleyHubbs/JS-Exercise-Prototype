4b6368e519b5249c6eb29034da34a520cdf3a0a6
BradleyHubbs <75264076+BradleyHubbs@users.noreply.github.com>
2020-12-16 17:06:14 -0500

    Finished Project

--- index.js
+++ index.js
@@ -39,9 +39,23 @@ function Airplane(name) {
           + It should return a string with `name` and `age`. Example: "Mary, 50"
   */
   
- function Person() {
-    
+ function Person(paramName, paramAge) {
+  this.name = paramName,
+  this.age = paramAge,
+  this.stomach = []
+}   
+Person.prototype.eat = function(someFood){
+  if (this.stomach.length < 10) {
+    this.stomach.push(someFood)
   }
+}
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+
+Person.prototype.toString = function(){
+ return `${this.name}, ${this.age}` 
+}
  
  
 
@@ -63,9 +77,16 @@ function Airplane(name) {
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
   
- function Car() {
-    
-  }
+ function Car(paramModel, paramMilesPerGallon) {
+  this.model = paramModel,
+  this.milesPerGallon = paramMilesPerGallon,
+ this.tank = 0,
+ this. odometer = 0
+}
+
+Car.prototype.fill = function(gallons){
+this.tank += gallons;
+}
   
   
   /*
@@ -75,18 +96,26 @@ function Airplane(name) {
       - Besides the methods on Person.prototype, babies have the ability to `.play()`:
           + Should return a string "Playing with x", x being the favorite toy.
   */
- function Baby() {
-   
-  }
+ function Baby(paramName, paramAge, paramFavoriteToy) {
+  this.name = paramName;
+  this.age = paramAge;
+  this.favoriteToy = paramFavoriteToy;
+  this.stomach = []
+ }
+
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+ return `Playing with ${this.favoriteToy}`
+}
  
   
   /* 
     TASK 4
     In your own words explain the four principles for the "this" keyword below:
-    1. 
-    2. 
-    3. 
-    4. 
+    1. Window/Global Object Binding
+    2. Implicit Binding
+    3. New Binding 
+    4. Explicit Binding 
   */
   
   
